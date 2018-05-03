0517f03c4a2f8c65ec8bd2dab42e63c73fa8b6cb
tomasgtz <tomasgtz@gmail.com>
2018-05-02 22:45:00 -0500

    Prueba de conflictos
    probar github

--- app/Controller/CountriesController.php
+++ app/Controller/CountriesController.php
@@ -48,11 +48,13 @@ class CountriesController extends AppController {
 	public function add() {
 		if ($this->request->is('post')) {
 			$this->Country->create();
-			if ($this->Country->save($this->request->data)) {
-				$this->Flash->success(__('The country has been saved.'));
-				return $this->redirect(array('action' => 'index'));
-			} else {
-				$this->Flash->error(__('The country could not be saved. Please, try again.'));
+			if(true) {
+				if ($this->Country->save($this->request->data)) {
+					$this->Flash->success(__('The country has been saved.'));
+					return $this->redirect(array('action' => 'index'));
+				} else {
+					$this->Flash->error(__('The country could not be saved. Please, try again.'));
+				}
 			}
 		}
 		$createdUsers = $this->Country->CreatedUser->find('list');
