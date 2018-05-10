95260de55964946e77cf368c7c0abdbd92c6aa41
RodriguezJaime <rodriguez.jaime2014@gmail.com>
2018-05-09 20:12:21 -0500

    Revert commit 6cf7b4cab732750b92d4053be0c79d8380f6aff2

--- app/Controller/Component/FileComponent.php
+++ app/Controller/Component/FileComponent.php
@@ -1,62 +1,65 @@
 <?php
+
 App::uses('Component', 'Controller');
 
 class FileComponent extends Component {
-    
+
     public $allowedExtensions = array('jpg', 'jpeg', 'gif', 'png', 'pdf', 'doc', 'docx', 'zip');
-    
     public $identifier = '';
-    
     public $maxSize = 5242880;
-    
-    public $route = APP . 'webroot' . DIRECTORY_SEPARATOR . 'files' . DIRECTORY_SEPARATOR;
+    public $routeToSave = APP . 'webroot' . DIRECTORY_SEPARATOR . 'files' . DIRECTORY_SEPARATOR;
+    public $name = '';
 
-    public $fileName = '';
-
-    public function getIdentifier(){
-        if($this->identifier == ''){
+    public function getIdentifier() {
+        if ($this->identifier == '') {
             return uniqid();
         } else {
             return $this->identifier;
         }
     }
 
-    public function extractExtesion($name){
+    public function extractExtesion($name) {
         return substr(strtolower(strrchr($name, '.')), 1);
     }
 
-    public function save($data){
+    public function save($data) {
 
         $name = ($data['name'] == null) ? '' : $data['name'];
-        
+
         $size = ($data['size'] == null) ? 0 : $data['size'];
 
         $tmp_name = ($data['tmp_name'] == null) ? '' : $data['tmp_name'];
 
-        if($name == '' || $size == 0 || $tmp_name == ''){
-            $this->fileName = '';
+        if ($name == '' || $size == 0 || $tmp_name == '') {
+            $this->name = '';
             return false;
         }
-        
+
         if (!in_array($this->extractExtesion($name), $this->allowedExtensions)) {
-            $this->fileName = '';
+            $this->name = '';
             return false;
         }
 
         if ($size > $this->maxSize) {
-            $this->fileName = '';
+            $this->name = '';
             return false;
         }
 
-        $fileName = $this->route . $this->getIdentifier() . $data['name'];
+        $name = $this->getIdentifier() . $data['name'];
+
+        $fullPathFileName = $this->routeToSave . $name;
+
         try {
-            if(move_uploaded_file($data['tmp_name'], $fileName)) {
-                $this->fileName = $fileName;
+            if (move_uploaded_file($data['tmp_name'], $fullPathFileName)) {
+                $this->name = $name;
                 return true;
+            } else {
+                return false;
             }
         } catch (Exception $e) {
-            $this->fileName = '';
+            $this->name = '';
             return false;
         }
     }
+
 }
